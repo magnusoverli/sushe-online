5990e99f1e571f496bd89eb45aeb6196015200d0
Magnus Øverli <magnus@overli.dev>
2025-06-01 20:36:11 +0200

    Remove duplicate code and unused code

--- settings-template.js
+++ settings-template.js
@@ -1,5 +1,6 @@
-// Import the header component at the top of the file
+// Import the header component and color utilities at the top of the file
 const { headerComponent } = require('./templates');
+const { adjustColor, colorWithOpacity } = require('./color-utils');
 
 // Settings page template
 const settingsTemplate = (req, options) => {
@@ -950,26 +951,5 @@ const settingsTemplate = (req, options) => {
 </html>
   `;
 };
-
-// Helper functions that need to be imported or defined
-function adjustColor(color, amount) {
-  const num = parseInt(color.replace("#", ""), 16);
-  const amt = Math.round(2.55 * amount);
-  const R = (num >> 16) + amt;
-  const G = (num >> 8 & 0x00FF) + amt;
-  const B = (num & 0x0000FF) + amt;
-  return "#" + (0x1000000 + (R < 255 ? R < 1 ? 0 : R : 255) * 0x10000 +
-    (G < 255 ? G < 1 ? 0 : G : 255) * 0x100 +
-    (B < 255 ? B < 1 ? 0 : B : 255))
-    .toString(16).slice(1);
-}
-
-function colorWithOpacity(color, opacity) {
-  const num = parseInt(color.replace("#", ""), 16);
-  const r = (num >> 16) & 255;
-  const g = (num >> 8) & 255;
-  const b = num & 255;
-  return `rgba(${r}, ${g}, ${b}, ${opacity})`;
-}
 
 module.exports = { settingsTemplate };
