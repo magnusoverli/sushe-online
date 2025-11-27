4df28f7be3edf2baf575152338b63179d17716c0
Magnus Overli <magnus.overli@gmail.com>
2025-11-27 10:24:46 +0100

    Reduce volume slider width for better spacing (w-28 → w-20)

--- templates.js
+++ templates.js
@@ -1100,7 +1100,7 @@ const spotifyTemplate = (user) => `
                 <i class="fas fa-volume-up text-xs"></i>
               </button>
               <input id="miniplayerVolume" type="range" min="0" max="100" value="50" 
-                class="w-28 h-1 bg-gray-700 rounded-full appearance-none cursor-pointer">
+                class="w-20 h-1 bg-gray-700 rounded-full appearance-none cursor-pointer">
             </div>
           </div>
           
